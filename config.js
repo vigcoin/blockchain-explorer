85bdcfeddc8574fb7bf1ba0020100b04efb191d1
bbscoindev <dev@bbscoin.xyz>
2018-05-09 23:42:09 -0700

    Update

--- config.js
+++ config.js
@@ -5,6 +5,12 @@ var symbol = 'BBS';
 var refreshDelay = 30000;
 var networkStat = {
     'bbs': [
-        ['pool.bbscoin.xyz', "http://pool.bbscoin.xyz:8117"]
+        ['pool.bbscoin.xyz', 'https://explorer.bbscoin.xyz'],
+        ['bbscoinpool.club', 'https://explorer.bbscoin.xyz/proxy/us.bbscoinpool.club:8117'],
+	['bbs.dreampool.info', 'https://bbs.dreampool.info/api'],
+	['cryptoknight.cc/bbs', 'https://explorer.bbscoin.xyz/proxy/78.46.85.142:2778'],
+	['bbs.gonspool.com', 'https://explorer.bbscoin.xyz/proxy/bbs.gonspool.com:8117'],
+	['pool-marine.ga/bbs', 'https://explorer.bbscoin.xyz/proxy/pool-marine.ga:8028'],
+	['bbs.howpool.com', 'https://explorer.bbscoin.xyz/proxy/bbs.howpool.com:8117']
     ]
 };
