031a51e3002e839a7317f97f63472f9ac8a3b064
calidion <calidion@gmail.com>
2019-01-29 13:36:37 +0800

    init commit

--- config.js
+++ config.js
@@ -1,16 +1,10 @@
-var api = 'https://explorer.bbscoin.xyz';
+var api = 'http://rpc.vigcoin.org:19801';
 var blockTargetInterval = 120;
 var coinUnits = 100000000;
-var symbol = 'BBS';
+var symbol = 'VIG';
 var refreshDelay = 30000;
 var networkStat = {
-    'bbs': [
-        ['pool.bbscoin.xyz', 'https://explorer.bbscoin.xyz'],
-        ['bbscoinpool.club', 'https://explorer.bbscoin.xyz/proxy/us.bbscoinpool.club:8117'],
-	['bbs.dreampool.info', 'https://bbs.dreampool.info/api'],
-	['cryptoknight.cc/bbs', 'https://explorer.bbscoin.xyz/proxy/78.46.85.142:2778'],
-	['bbs.gonspool.com', 'https://explorer.bbscoin.xyz/proxy/bbs.gonspool.com:8117'],
-	['pool-marine.ga/bbs', 'https://explorer.bbscoin.xyz/proxy/pool-marine.ga:8028'],
-	['bbs.howpool.com', 'https://explorer.bbscoin.xyz/proxy/bbs.howpool.com:8117']
-    ]
+	'vig': [
+		['vig.tyk.im', 'https://vig.tyk.im']
+	]
 };
