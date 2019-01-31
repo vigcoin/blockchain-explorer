92e64dbd997b1885d0588f6f22c6c803d090cfb0
calidion <calidion@gmail.com>
2019-01-31 17:22:17 +0800

    fixed percent problem

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
 var api = 'https://rpc.vigcoin.org';
+// var api = 'http://rpc.vigcoin.org:19801';
 var blockTargetInterval = 120;
 var coinUnits = 100000000;
+var MAX_SUPPLY = 100000000;
 var symbol = 'VIG';
 var refreshDelay = 30000;
 var networkStat = {
